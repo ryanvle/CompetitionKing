da36b2ca28c96c2d36eaf451fd4e641520b698cf
RVL <91763551+ryanvle@users.noreply.github.com>
2023-06-28 15:13:30 -0700

    Fixed SQL

--- SQL/Populate Type Tables.sql
+++ SQL/Populate Type Tables.sql
@@ -1,20 +1,5 @@
 --tblGENDER
 INSERT INTO tblGENDER(GenderName)
-VALUES ('Male')
-
-INSERT INTO tblGENDER(GenderName)
-VALUES ('Female')
-
---tblLEVEL
-INSERT INTO tblLEVEL(LevelName)
-VALUES ('Beginner')
-
-INSERT INTO tblLEVEL(LevelName)
-VALUES ('Intermediate')
-
-INSERT INTO tblLEVEL(LevelName)
-VALUES ('Advance')
-=======
 VALUES ('Male');
 
 INSERT INTO tblGENDER(GenderName)
@@ -46,4 +31,3 @@ VALUES('Compulsory Long Fist');
 
 INSERT INTO tblFORM(FormName)
 VALUES('Compulsory Southern Fist');
-
