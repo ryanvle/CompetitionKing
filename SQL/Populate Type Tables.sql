8c727f7d66a17490638b0286dad607ed8e09d8cf
ryanvle <ryanvle@uw.edu>
2023-06-27 20:21:13 -0700

    Made and Populated tblSTYLE and tblFORM

--- SQL/Populate Type Tables.sql
+++ SQL/Populate Type Tables.sql
@@ -15,4 +15,20 @@ VALUES ('Intermediate');
 INSERT INTO tblLEVEL(LevelName)
 VALUES ('Advance');
 
+--tblSTYLE
+INSERT INTO tblSTYLE(StyleName)
+VALUES ('Wushu');
+
+INSERT INTO tblSTYLE(StyleName)
+VALUES ('Traditional');
+
+INSERT INTO tblSTYLE(StyleName)
+VALUES ('Taichi');
+
+--tblFORM
+INSERT INTO tblFORM(FormName)
+VALUES('Compulsory Long Fist');
+
+INSERT INTO tblFORM(FormName)
+VALUES('Compulsory Southern Fist');
 
