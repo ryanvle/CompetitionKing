870b440fe396de6641c666fb553da9834aca72ca
= <=>
2023-06-27 20:18:45 -0700

    DB Competition Addition

--- src/api-routes/CompetitionRoutes.js
+++ src/api-routes/CompetitionRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const dbcomp = require("../services/Competition")
 
-
-router.post('/CreateNewCompetition', function (req, res) {
-    console.log(req.query)
-    res.send('Create New Competition Controller');
+router.post('/CreateNewCompetition', async function (req, res) {
+    await dbcomp.CreateNewCompetition(req.query["name"], req.query["description"], req.query["fees"]);
+    res.send("sent");
 });
 
 router.delete('/DeleteCompetition', function (req, res) {
-    res.send('Delete Competition Controller');
+    res.status(200).send('Delete Competition Controller');
 });
 
 module.exports = router;
--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ const express = require("express")
 const app = express()
 const userRoutes = require('./api-routes/userRoutes')
 const CompetitionRoutes = require('./api-routes/CompetitionRoutes')
+const queryparser = require("express-query-int")
 
 app.use(express.urlencoded())
 app.use(express.json())
-
+app.use(queryparser())
 
 app.use('/users', userRoutes);
 app.use('/Competition', CompetitionRoutes)
