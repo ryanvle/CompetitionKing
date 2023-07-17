5ec5a9b5c43af5032cc13e33d6a65abeface0c86
FrankWuuuu <86853681+FrankWuuuu@users.noreply.github.com>
2023-07-17 01:25:14 -0700

    finishedEvents 50%

--- client/src/components/competition/finishedEventsColumn.tsx
+++ client/src/components/competition/finishedEventsColumn.tsx
@@ -1,33 +1,14 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import FutureEventsList from '../../components/competition/futureEventsList';
-import CurrentlyGoingStaff, { competitor } from '../../components/competition/currentlyGoingStaff';
-import DoneList from '../../components/competition/doneList';
 import DoneEvent from '../../components/competition/doneEvent';
+import { wushuEvent } from '../../pages/competition/staff';
 
-export type wushuEvent= {
-    eventName:string;
-    competitors: competitor[]
+
+interface Props{
+    ringNumber: number
 }
-
-const Staff = () => {
-    const queryParams = new URLSearchParams(window.location.search);
-    const ringNumQuery = queryParams.get("ringNumber");
-    if(ringNumQuery === null) throw Error;
-    const ringNumber:number =  Number.parseInt(ringNumQuery);
-
-
-    const [futureEventNames, setFutureEventNames] = useState<string[]>([
-        "Advanced Adult Female Changquan",
-        "Advanced Adult Female DaoShu",
-        "Advanced Adult Female GunShu",
-    ])
-    const [futureEventsCompetitors, setFutureEventsCompetitors] = useState<string[][]>([
-        ["Qoey","Woey","Roey","Toey",],
-        ["Yoey","Poey","Soey","Loey",],
-        ["Foey","Goey","Hoey","Joey",],
-    ])
-
+const FinishedEventsColumn = (props:Props) => {
+    const ringNumber = props.ringNumber;
+    
     const [doneData, setDoneData] = useState<wushuEvent[]>([
         {
             eventName: "Five Stance - Beginner Adult Men",
@@ -67,11 +48,7 @@ const Staff = () => {
 
     ])
 
-
-    let upNextElement:JSX.Element[] = futureEventsCompetitors[0].map(competitor=>
-        <p key={"comp"+competitor}>{competitor}</p>)
-
-    const doneElement: JSX.Element[]=[];
+  const doneElement: JSX.Element[]=[];
     for (let i = 0; i < doneData.length; i++){
         const doneEventData = doneData[i];
         const sortedData = doneEventData.competitors.slice();
@@ -83,28 +60,12 @@ const Staff = () => {
             />
         );
     }
-
-
-    const curlength:number = futureEventsCompetitors[0].length
-    return (
-        <div>
-            <Link to={"/competition/manageRings"}>Back </Link>
-            <h2>Currently Going:</h2>
-                <CurrentlyGoingStaff ringNumber={ringNumber}/>
-
-            <h2>Up Next:</h2>
-                <h3>{futureEventNames[0]} {curlength}/{curlength} </h3>
-                {upNextElement}
-            <h2>Remaining Events ({futureEventNames.length})</h2>
-                <FutureEventsList
-                    futureEventNames={futureEventNames.slice(1)}
-                    futureEventsCompetitors= {futureEventsCompetitors.slice(1)}
-                />
-
-            <h2>Finished Events</h2>
-            {doneElement}
-        </div>
-    )
+  return (
+    <div>
+        <h3>Finished Events:</h3>
+        {doneElement}
+    </div>
+  )
 }
 
-export default Staff
+export default FinishedEventsColumn
